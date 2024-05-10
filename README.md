d7718f88cf24bd8b2abfb0efd662f126f3e6f023
Andreas Gottardi <andreas@goa.systems>
2024-05-10 19:04:04 +0200

    Switching to Kotlin script based build definitions.

--- README.md
+++ README.md
@@ -23,5 +23,7 @@ dependencies {
 
 |Version|Description|
 |-|-|
-|0.3.1|Migration to GitHub complete. Dependency updates.|
+|0.5.1|Migrating to Kotlin based Gradle scripts.|
+|0.5.0|Adding logic.|
 |0.4.0|Adding logic to make a given string valid.|
+|0.3.1|Migration to GitHub complete. Dependency updates.|
---
+++ build.gradle.kts
@@ -0,0 +1,155 @@
+plugins {
+	`java-library`
+	`maven-publish`
+    `jacoco`
+}
+
+group = "goa.systems"
+version = project.property("ARTIFACT_VERSION").toString()
+
+val fullSetup by configurations.creating {
+	extendsFrom(configurations["testImplementation"])
+}
+
+java {
+	sourceCompatibility = JavaVersion.VERSION_11
+	targetCompatibility = JavaVersion.VERSION_11
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+
+	implementation("org.slf4j:slf4j-api:2.0.12")
+	
+	/* Specify all dependencies in configuration fullSetup that are conveniently used during development and
+	   that allow execution of the application but which are optional and up to the customer to define. */
+	fullSetup("ch.qos.logback:logback-core:1.5.3") {
+		exclude(group = "org.slf4j", module = "slf4j-api")
+	}
+	fullSetup("ch.qos.logback:logback-classic:1.5.3") {
+		exclude(group = "org.slf4j", module = "slf4j-api")
+	}
+	testImplementation("org.junit.jupiter:junit-jupiter:5.10.2")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+	withSourcesJar()
+	withJavadocJar()
+}
+
+tasks.named<Test>("test") {
+	useJUnitPlatform()
+	finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.register<Copy>("exportPom") {
+
+	group = "build"
+	description = "Copy application libraries"
+	doLast {
+		from("build/publications/maven/pom-default.xml")
+		into("build/export/conf")
+	}
+}
+
+tasks.register<Copy>("exportLogbackConfig") {
+
+	group = "build"
+	description = "Copy application libraries"
+	from("src/test/resources")
+	include("logback-test.xml")
+	into(layout.buildDirectory.dir("export/conf"))
+	rename("logback-test\\.xml", "logback.xml")
+}
+
+tasks.register<Copy>("exportReadme") {
+
+	group = "build"
+	description = "Copy application libraries"
+	
+	from(".")
+	into(layout.buildDirectory.dir("export"))
+	include("README.md")
+}
+
+tasks.register<Copy>("exportApplicationLibraries") {
+
+	group = "build"
+	description = "Copy application libraries"
+	dependsOn(tasks["build"])
+	dependsOn(tasks["jar"])
+	dependsOn(tasks["javadocJar"])
+	dependsOn(tasks["sourcesJar"])
+	
+	from(layout.buildDirectory.dir("libs"))
+	into(layout.buildDirectory.dir("export/lib"))
+	include("*.jar")
+	
+}
+
+tasks.register<Copy>("exportFullSetupDependencies") {
+
+	group = "build"
+	description = "Copy full setup dependencies"
+	
+	from(configurations.runtimeClasspath)
+	from(configurations["fullSetup"])
+    into(layout.buildDirectory.dir("export/lib"))
+	
+}
+
+tasks.register("writeVariables") {
+
+	group = "build"
+	description = "Writes variables for maven"
+	
+	doLast {
+		with(layout.buildDirectory.file("export/vars").get().asFile) {
+            parentFile.mkdirs()
+            writeText("export VERSION=${version}\nexport GROUP=goa.systems\nexport ARTIFACT=commons")
+        }
+	}
+}
+		
+tasks.register<Tar>("distribute") {
+
+	group = "build"
+	description = "Creates tgz distribution."
+	dependsOn(tasks["exportReadme"])
+	dependsOn(tasks["exportPom"])
+	dependsOn(tasks["writeVariables"])
+	dependsOn(tasks["exportLogbackConfig"])
+	dependsOn(tasks["exportFullSetupDependencies"])
+	dependsOn(tasks["exportApplicationLibraries"])
+	
+	from(layout.buildDirectory.dir("export"))
+    archiveExtension.set("tar.gz")
+	compression = Compression.GZIP
+	
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("Commons") {
+            groupId = "goa.systems"
+            artifactId = "commons"
+            version = version
+            from(components["java"])
+            pom {
+				name = "GOA systems commons"
+				description = "A collection of library functions."
+			}
+        }
+    }
+}
+
+tasks.jacocoTestReport {
+	dependsOn(tasks.test)
+	reports {
+		xml.required = true
+    }
+}
