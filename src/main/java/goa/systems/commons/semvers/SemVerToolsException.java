a0598bf52cb348f3e56969d76f1e01b550eedd70
AndreasGottardi <andreas@goa.systems>
2024-09-28 12:25:59 +0200

    Adding semantic version handling.

---
+++ src/main/java/goa/systems/commons/semvers/SemVerToolsException.java
@@ -0,0 +1,10 @@
+package goa.systems.commons.semvers;
+
+public class SemVerToolsException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public SemVerToolsException(String message) {
+		super(message);
+	}
+}
